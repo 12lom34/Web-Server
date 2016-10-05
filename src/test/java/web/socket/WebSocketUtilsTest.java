336b83455b12f5c53bce9240f604f3f47bae519c
Alex Likho <12.lom.34@gmail.com>
2016-10-05 22:43:43 +0300

    Add stub unit test

---
+++ src/test/java/web/socket/WebSocketUtilsTest.java
@@ -0,0 +1,14 @@
+package web.socket;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WebSocketUtilsTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
