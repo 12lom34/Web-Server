e12b98862756bbb2ee8cfaf736827530692d0440
Alex Likho <12.lom.34@gmail.com>
2016-10-17 23:23:40 +0300

    Add first attempt of SocketChannel client-server implementation.

--- src/main/java/web/socket/StreamServerNIOBlocking.java
+++ src/main/java/web/socket/StreamServerNIOBlocking.java
@@ -14,17 +14,18 @@ public class StreamServerNIOBlocking {
 	private static final Charset charset = Charset.forName("ISO-8859-1");
 	private static final CharsetDecoder decoder = charset.newDecoder();
 	private static final CharsetEncoder encoder = charset.newEncoder();
-	
+
 	public static void main(String[] args) throws IOException {
 		if (args.length != 1) {
-			WebSocketUtils.log("Usage: java NIOServerBlockingImplementation <port number>"); System.exit(1);
+			WebSocketUtils.log("Usage: java NIOServerBlockingImplementation <port number>");
+			System.exit(1);
 		}
 
 		while (true) {
 			try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
 				serverSocketChannel.socket().bind(new InetSocketAddress(Integer.parseInt(args[0])));
 				try (SocketChannel clientSocket = serverSocketChannel.accept()) {
-					 handle(clientSocket);
+					handle(clientSocket);
 				}
 			}
 		}
@@ -34,41 +35,41 @@ public class StreamServerNIOBlocking {
 		return inputLine.toUpperCase();
 	}
 
-	private static void handle(SocketChannel socketChannel) {
+	private static void handle(SocketChannel socketChannel) throws IOException {
 		ByteBuffer buffer = ByteBuffer.allocate(1024);
-		CharBuffer charBuffer = CharBuffer.allocate(1024);
-//		try (CharBuffer buffer = new CharBuffer(80)) {
-			int byteData;
-			while ((byteData = socketChannel.read(buffer)) != -1) {
-			      buffer.flip();
-			      decoder.decode(buffer, charBuffer, false);
-			      charBuffer.flip();
-			      WebSocketUtils.log(String.format("Client request (%s). Message : %s.", WebSocketUtils.getCurrentTimeInUTC(), charBuffer));
-//			      transform(charBuffer);
-			      System.out.println(charBuffer);
-			      buffer.clear();
-			      charBuffer.clear();
-			    }
-			    
-			    
-//			}
-//		} catch (IOException e) {
-//			WebSocketUtils.log("Exception caught when trying to listen on port " + socketChannel.getLocalAddress() + " or listening for a connection");
-//			WebSocketUtils.log(e.getMessage());
+		CharBuffer charBuffer;
+		int byteData;
+		while ((byteData = socketChannel.read(buffer)) != -1) {
+			buffer.flip();
+			// decoder.decode(buffer, charBuffer, false);
+			charBuffer = charset.decode(buffer);
+			// charBuffer.flip();
+			WebSocketUtils.log(String.format("Client request (%s). Message : %s.", WebSocketUtils.getCurrentTimeInUTC(),
+					charBuffer));
+			// transform(charBuffer);
+			System.out.println(charBuffer);
+			byte[] byteArray = new byte[1024];
+			buffer.get(byteArray);
+			System.out.println("Server got from client the string: " + new String(byteArray));
+			String an = new String(byteArray);
+			buffer.clear();
+			charBuffer.clear();
 		}
-		
-//		
-//
-//        while ((data = s.read(buffer)) != -1) {
-//
-//            buffer.flip();
-//            transform(buffer);
-//
-//            while (buffer.hasRemaining()) {
-//                s.write(buffer);
-//
-//            }
-//            buffer.compact();
-//        }
-//	}
+
+	}
+
+	//
+	//
+	// while ((data = s.read(buffer)) != -1) {
+	//
+	// buffer.flip();
+	// transform(buffer);
+	//
+	// while (buffer.hasRemaining()) {
+	// s.write(buffer);
+	//
+	// }
+	// buffer.compact();
+	// }
+	// }
 }
