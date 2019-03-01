85847c55cc89b16008f62ed3a16c04add3baf73e
Alex Likho <oleksandr.likho@microfocus.com>
2019-03-01 16:41:32 +0200

    Update test 1.

--- src/test/java/web/socket/WebSocketUtilsTest.java
+++ src/test/java/web/socket/WebSocketUtilsTest.java
@@ -6,8 +6,15 @@ import org.junit.Test;
 
 public class WebSocketUtilsTest {
 
+	
 	@Test
 	public void test() {
+		
+		if(true) {
+			System.out.println("Always here.");
+		} else {
+			System.out.println("Never reach here.");
+		}
 		assertTrue(true);
 	}
 
